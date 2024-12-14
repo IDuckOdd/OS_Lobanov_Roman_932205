9fa477157a7a5b2a673caaacf3f7792fd178bf47
IDuckOdd <79915759+IDuckOdd@users.noreply.github.com>
2024-12-14 15:42:31 +0700

    Update prod_con.c

--- prod_con.c
+++ prod_con.c
@@ -3,36 +3,26 @@
 #include <pthread.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1
-
-typedef struct {
-    char *data;
-} Event;
-
-Event buffer[BUFFER_SIZE];
-int count = 0;
+int ready = 0;
 
 pthread_mutex_t mutex;
 pthread_cond_t cond_var;
 
 void* producer(void* arg) {
-    while (1) {
+    while(1) {
         sleep(1); //delay 1 sec
-        Event event;
-        event.data = "Event send";
 
         pthread_mutex_lock(&mutex);
-        
+
         //Wait free place
-        while (count == BUFFER_SIZE) {
+        while (ready == 1) {
             pthread_cond_wait(&cond_var, &mutex);
         }
 
-        buffer[count] = event;
-        count++;
-        printf("%s\n", event.data);
+        ready = 1;
+        printf("%s\n", "Event send");
 
-        pthread_cond_signal(&cond_var); // Notify consumer
+        pthread_cond_signal(&cond_var); //Notify consumer
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -40,17 +30,18 @@ void* producer(void* arg) {
 
 void* consumer(void* arg) {
     while (1) {
+
         pthread_mutex_lock(&mutex);
-        
+
         //Wait until appears
-        while (count == 0) {
+        while (ready == 0) {
             pthread_cond_wait(&cond_var, &mutex);
         }
 
         printf("Event taken\n");
-        count--;
+        ready = 0;
 
-        pthread_cond_signal(&cond_var); // Notify producer
+        pthread_cond_signal(&cond_var); //Notify producer
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -66,10 +57,6 @@ int main() {
     pthread_create(&consumer_thread, NULL, consumer, NULL);
 
     pthread_join(producer_thread, NULL);
-    pthread_join(consumer_thread, NULL);
-
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond_var);
 
     return 0;
 }
